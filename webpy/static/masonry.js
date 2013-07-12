421c42df5977e7fa84ac07cc6a8addc5579bdaac
hepengfei <hepengfei@xunlei.com>
2013-07-12 08:39:24 +0800

    完成了瀑布流显示功能；完成了无限滚动功能

--- webpy/static/masonry.js
+++ webpy/static/masonry.js
@@ -1,4 +1,5 @@
 
+var picfixwidth=300
 
 
 function load_pic(arr)
@@ -14,7 +15,9 @@ function load_pic(arr)
         var img=document.createElement("img");
         $(img).attr("src", "/pic/file/" + info.picid + "." + info.picformat.toLowerCase())
             .attr("title", info.picname)
-            .attr("alt", info.picname);
+            .attr("alt", info.picname)
+            .attr("width", picfixwidth + "px")
+            .attr("height", Math.round(info.picwidth * picfixwidth / info.picheight) + "px");
 
         var a=document.createElement("a");
         $(a).attr("href", "/pic/file/" + info.picid + "." + info.picformat.toLowerCase());
@@ -50,7 +53,7 @@ function load_pic(arr)
                 .masonry();
         }
     },
-                      500);
+                      00);
 }
 
 
@@ -70,6 +73,12 @@ function load_next_page()
     jsonurl="/pic/info/list/" + cur_page + "/" + num_per_page + "?callback=load_pic";
     exejson(jsonurl);
     cur_page += num_per_page;
+}
+
+function set_next_page()
+{
+    nexturl="/pic/info/list/" + cur_page + "/" + num_per_page;
+    $("a#more:last").attr('href',nexturl);
 }
 
 load_next_page();
